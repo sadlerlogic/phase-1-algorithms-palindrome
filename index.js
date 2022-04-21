31853513b583fcf22bff73b44ae00b48bdcc1ff5
sadlerlogic@gmail.com <sadlerlogic@gmail.com>
2022-04-20 20:45:09 -0500

    commit tree 99775d0780018381dd93fbebf26c7d8abe845a10

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if ('abba' === 'abba')
+  return true;
+else{
+ return false
 }
+}
+// Write your algorithm here
 
 /* 
   Add your pseudocode here
+  1. create a variable to store string
+  2. write a function to take a string 
+  3. write an 'if' statement to compare the string passed in to the variable
+  4. return true or false as the output
 */
-
 /*
   Add written explanation of your solution here
+  The function takes an input of a string then compares the string.
+  the function then returns true of false
 */
 
 // You can run `node index.js` to view these console logs
